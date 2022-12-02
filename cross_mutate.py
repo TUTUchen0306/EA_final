2fdc870bdcaa8e62ab1b4e543de4ac1c611082ea
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2022-12-02 22:15:49 +0800

    add noise ok

--- cross_mutate.py
+++ cross_mutate.py
@@ -1,16 +1,41 @@
 from util import read_img
 import random
 import numpy as np
+import numpy as np
+import os
+import cv2
+from PIL import Image
+
+# gaussian noise
+def noisy(image):
+    row, col, ch = image.shape
+    mean = 0
+    sigma = 10
+    gauss = np.random.normal(mean, sigma, (row, col, ch))
+    gauss = gauss.reshape(row, col, ch)
+    noisy = (image + gauss).astype(int)
+    noisy = np.clip(noisy, 0, 255)
+    return noisy
 
 
-def init_gen():
-    arr = []
-    for i in range(512 * 512 * 3):
-        tmp = random.randint(0, 255)
-        arr.append(tmp)
-    arr = np.asarray(arr)
-    arr = arr.reshape(1, 3, 512, 512)
-    return arr
+def noisy_img(file_name):
+    pic = read_img(file_name)
+    # shape to 512, 512, 3
+    pic_reshape = np.zeros((512, 512, 3))
+    pic_reshape[:, :, 0] = pic[0][0]
+    pic_reshape[:, :, 1] = pic[0][1]
+    pic_reshape[:, :, 2] = pic[0][2]
+    # # show original image
+    # Image.fromarray(pic_reshape.astype("uint8"), "RGB").show()
+    newpic = noisy(pic_reshape)
+    # shape to 1, 3, 512, 512
+    newpic_reshape = np.zeros((1, 3, 512, 512))
+    newpic_reshape[0, 0, :, :] = newpic[:, :, 0]
+    newpic_reshape[0, 1, :, :] = newpic[:, :, 1]
+    newpic_reshape[0, 2, :, :] = newpic[:, :, 2]
+    # # show noisy image
+    Image.fromarray(newpic.astype("uint8"), "RGB").show()
+    return newpic_reshape
 
 
 # 1 point crossover with probability p = 1.0, no mutation operator
@@ -20,19 +45,20 @@ def crossover(arr_left, arr_right):
     arr_right = arr_right.ravel().tolist()
     # cut between cutPoint and cutPoint - 1
     cutPoint = random.randint(1, len(arr_left) - 1)
-    # print(cutPoint)
+    print(cutPoint)
     lL, lR = arr_left[:cutPoint], arr_left[cutPoint:]
     rL, rR = arr_right[:cutPoint], arr_right[cutPoint:]
     lNew = lL + rR
     rNew = rL + lR
-    # print(len(lL), len(rR), len(lL + rR), len(rL), len(lR), len(rL + lR))
+    print(len(lL), len(rR), len(lL + rR), len(rL), len(lR), len(rL + lR))
     lNew = np.asarray(lNew).reshape(1, 3, 512, 512)
     rNew = np.asarray(rNew).reshape(1, 3, 512, 512)
     return lNew, rNew
 
 
 # generate random (1, 3, 512, 512) picture
-pic = init_gen()
-pic1, pic2 = crossover(pic, pic)
-# print(pic)
-print(pic1.shape)
+pic1 = read_img("lighthouse.png")
+pic2 = read_img("starry.jpg")
+# shape (1, 3, 512, 512)
+new_pic1 = noisy_img("lighthouse.png")
+new_pic2 = noisy_img("starry.jpg")
--- util.py
+++ util.py
@@ -2,36 +2,36 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 
+
 def read_img(file_name, height=512, width=512):
-	image = Image.open(file_name)
+    image = Image.open(file_name)
 
-	image = image.convert('RGB')
+    image = image.convert("RGB")
 
-	loader=transforms.Compose([transforms.Resize((height, width))])
-	
-	image = loader(image)
+    loader = transforms.Compose([transforms.Resize((height, width))])
 
-	image = np.asarray(image)
+    image = loader(image)
 
-	image_array = []
-	red_array, green_array, blue_array = [], [], []
+    image = np.asarray(image)
+    image_array = []
+    red_array, green_array, blue_array = [], [], []
 
-	for h in range(height):
-		tr, tg, tb = [], [], []
-		for w in range(width):	
-			r, g, b = image[h][w]
-			tr.append(r)
-			tg.append(g)
-			tb.append(b)
+    for h in range(height):
+        tr, tg, tb = [], [], []
+        for w in range(width):
+            r, g, b = image[h][w]
+            tr.append(r)
+            tg.append(g)
+            tb.append(b)
 
-		red_array.append(tr)
-		green_array.append(tg)
-		blue_array.append(tb)	
+        red_array.append(tr)
+        green_array.append(tg)
+        blue_array.append(tb)
 
-	image_array.append(red_array)
-	image_array.append(green_array)
-	image_array.append(blue_array)
+    image_array.append(red_array)
+    image_array.append(green_array)
+    image_array.append(blue_array)
 
-	image_array = np.array([image_array])
+    image_array = np.array([image_array])
 
-	return image_array
+    return image_array
